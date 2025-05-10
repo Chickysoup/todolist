aa6fbc69077925eafc14d640ad0f5ec087e80fbd
MikeH <michaelhewitt135@gmail.com>
2025-05-09 22:26:51 -0700

    added delete, add, and checkoff functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,76 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [newToDoText, setToDoText] = useState('')
+
+  const AddToList = () => {
+    if (newToDoText == ''){return;}
+    setToDo([
+      ...toDo,
+      {
+        id: Date.now(),
+        text:newToDoText,
+        completed: false
+      }
+    ])
+    setToDoText('')
+  }
+
+  const toDoDelete = (id) => {
+    setToDo(toDo.filter(td => td.id !== id))
+  }
+
+  const toDoToggle = (id) => {
+    //console.log(toDo.id.text)
+    const toDoItem = toDo.find(td => td.id === id)
+    console.log(toDoItem.text)
+    
+    setToDo(
+      toDo.map( td =>
+        td.id === id ? {...td, completed: !td.completed}
+        : td
+      )
+    )
+  }
   
+  //initial to do list. array of objects
+  const [toDo, setToDo] = useState([
+    {id: 0, text : "first", completed : false},
+    {id: 1, text : "second", completed : false},
+    {id: 2, text : "third", completed : false},
+    {id: 3, text : "fourth", completed : false}
+  ])
 
   return (
     <>
-    <h1>hello world</h1>
+    <h1>To-do list</h1>
+    <input
+      type = "text"
+      value = {newToDoText}
+      onChange = {(e) => setToDoText(e.target.value)}
+      placeholder= "lol"
+      />
+    <button onClick={AddToList}>add</button>
+
+    <ul>
+      {toDo.map(td => (
+        <li
+          className={td.completed ? "completed" : ''}
+          key={td.id}
+          ><input 
+              type="checkbox"
+              checked = {td.completed}
+              onChange={ () => toDoToggle(td.id)}>
+            </input>
+              
+              {td.text}
+
+            <button
+              onClick={() => toDoDelete(td.id)}>delete</button>  
+              </li>
+      ))}
+    </ul>
+
     </>
   )
 }
